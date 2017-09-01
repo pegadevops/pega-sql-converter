13b53664479389b00d3e967d3121ebd24632f6f1
kxmxpxtx <virusmater@gmail.com>
2017-09-01 17:11:47 +0200

    Fix problem with NULL and add convert for dates

--- converter.py
+++ converter.py
@@ -1,16 +1,24 @@
+import re
+
 def convert(sqlText, sqlInserts):
+    rex = re.compile("^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d*")
     start = sqlInserts.find('<')
     while start >= 0:
         end = sqlInserts.find('>', start)
         if (end == -1):
             break
         insert = sqlInserts[start + 1:end]
-        # if is NULL that surrounded with double brackets
-        if insert[0] == "<":
-            insert = insert.replace('<', '')
-        # else surround with quotes
+        # if date
+        if rex.match(insert):
+            insert = 'TO_DATE(\'' + insert[0: 19] + '\', \'YYYY-MM-DD HH24:MI:SS\')'
         else:
-            insert = '\'' + insert + '\''
+            # if is NULL that surrounded with double brackets
+            if insert[0] == "<":
+                insert = insert.replace('<', '')
+                end += 1
+            # else surround with quotes
+            else:
+                insert = '\'' + insert + '\''
         if '&' in insert:
             insert = insert.replace('&', '\'|| chr(38) || \'')
         sqlText = sqlText.replace('?', insert, 1)
